f6e13b3be61f1ef799a3f7f67f7a6dd004cf8d83
Augusto Goulart <goulart.me@gmail.com>
2016-12-01 21:36:44 -0200

    add visibility example

---
+++ scripts/04_scope_visibility/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/agoulartx/gopher-padawan/scripts/03_packages/libtest"
+)
+
+func main() {
+	fmt.Println(vis.MyName)
+	vis.PrintVar()
+}
---
+++ scripts/04_scope_visibility/vis/name.go
@@ -0,0 +1,7 @@
+package vis
+
+
+// MyName is exported because it starts with a capital letter
+var MyName = "Todd"
+var yourName = "Future Rock Star Programmer"
+

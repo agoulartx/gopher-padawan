58f5c8456010818c323298b2c6ba345b5126d127
Augusto Goulart <goulart.me@gmail.com>
2016-12-02 16:34:34 -0200

    swtich case section added

---
+++ scripts/12_switch_Case/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Single evaluation
+	nome := "mario"
+	switch nome {
+	case "jhon":
+		fmt.Println("Welcome Jhon")
+	case "mario":
+		fmt.Println("Welcome Mario")
+	default:
+		fmt.Println("Nenhum")
+
+	}
+
+	// Multiple evaluation
+	name := "maria"
+	switch name {
+	case "jhon","carlos":
+		fmt.Println("Welcome Jhon")
+	case "rosa","maria":
+		fmt.Println("Welcome Maria")
+	default:
+		fmt.Println("Nenhum")
+
+	}
+}

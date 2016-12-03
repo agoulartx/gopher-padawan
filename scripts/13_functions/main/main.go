7ed34657d8e9b3c30cc9b95cfa364c9be54d6b5f
Augusto Goulart <goulart.me@gmail.com>
2016-12-03 16:14:52 -0200

    add variadic function section

--- scripts/13_functions/main/main.go
+++ scripts/13_functions/main/main.go
@@ -6,11 +6,21 @@ import (
 )
 
 func main(){
+	// Arguments
 	functions.Singleprint("Hello!")
 	functions.Multiprint("Welcome,","Jhon")
+	fmt.Println("-------------------")
+
+	// Returns
 	a := functions.Greet("Augusto","Goulart")
 	fmt.Println(a)
-
 	fmt.Println(functions.Namedgreet("Jhon","Doe"))
 	fmt.Println(functions.Doublereturn("Carlos","Mario"))
+	fmt.Println("-------------------")
+
+	// Variadic
+	v := functions.Variadicsum(188.4,487.6,8.9,222.1,543.6)
+	fmt.Println(v)
+	fmt.Println("-------------------")
+
 }

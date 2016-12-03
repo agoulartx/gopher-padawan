bc611b136d03eede69f241ed73313f0e9ec8447b
Augusto Goulart <goulart.me@gmail.com>
2016-12-03 15:12:13 -0200

    add returns section on functions

--- scripts/13_functions/main/main.go
+++ scripts/13_functions/main/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/agoulartx/gopher-padawan/scripts/13_functions/13_1_functions"
+import (
+	"github.com/agoulartx/gopher-padawan/scripts/13_functions"
+	"fmt"
+)
 
 func main(){
-	arguments.Myprint("Welcome,","Jhon")
+	functions.Singleprint("Hello!")
+	functions.Multiprint("Welcome,","Jhon")
+	a := functions.Greet("Augusto","Goulart")
+	fmt.Println(a)
+
+	fmt.Println(functions.Namedgreet("Jhon","Doe"))
+	fmt.Println(functions.Doublereturn("Carlos","Mario"))
 }

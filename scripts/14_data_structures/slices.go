1adb715d3a45a09da5d41ced9dc19d6f9dcc11f3
Augusto Goulart <goulart.me@gmail.com>
2016-12-05 16:22:43 -0200

    add slices section

--- scripts/14_data_structures/slices.go
+++ scripts/14_data_structures/slices.go
@@ -5,14 +5,29 @@ import "fmt"
 func Showslice() {
 	mySlice := []int{1,2,3,4,5,6,7,8,9,0}
 	fmt.Println(mySlice)
+	fmt.Println("-----------")
 
 	// Slicing
 
 	fmt.Println(mySlice[2:5])
 
 	// Access by index
+	fmt.Println("-----------")
 
-	fmt.Println(mySlice[0])
-	fmt.Println("mySlice"[4])
+	fmt.Println(mySlice[4])
+	fmt.Println("-----------")
+
+	newSlice := make([]int, 0, 5)
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+	fmt.Println("-----------")
+
+	for i := 0; i<80;i++{
+		newSlice = append(newSlice, i)
+		fmt.Println("Len:", len(newSlice), "Cap:",cap(newSlice), "Value:", newSlice[i])
+	}
+
 
 }
